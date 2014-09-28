948d5ac01ffea33ec096fb2fe0e8d2da44eca386
Yuichi TANIKAWA <t.yuichi111@gmail.com>
2014-09-29 02:33:28 +0900

    Add role brew

--- roles/linuxbrew/files/linuxbrew.sh
+++
@@ -1,6 +0,0 @@
-export PATH=$HOME/.linuxbrew/bin:$PATH
-export LD_LIBRARY_PATH=$HOME/.linuxbrew/lib:$LD_LIBRARY_PATH
-
-# rbenv
-export RBENV_ROOT=$HOME/.linuxbrew/var/rbenv
-if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi
