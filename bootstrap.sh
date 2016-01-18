18894bc847913a7472ef3d83d6bd2ea07b9f68c0
Yuichi Tanikawa <t.yuichi111@gmail.com>
2016-01-19 01:06:25 +0900

    Remove unused files

--- bootstrap.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -eu
-
-curl https://bootstrap.pypa.io/get-pip.py | sudo python
-sudo pip install ansible
--- tests/test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-set -ex
-ansible-galaxy install -r requirements.yml
-ansible-playbook site.yml -vv
