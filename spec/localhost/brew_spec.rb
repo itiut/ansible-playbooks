7d20cee18af6cba3771481cbe191f4d56cc7f6ec
Yuichi Tanikawa <t.yuichi111@gmail.com>
2016-01-19 10:30:07 +0900

    Use constants

--- spec/localhost/brew_spec.rb
+++ spec/localhost/brew_spec.rb
@@ -1,11 +1,14 @@
 require 'spec_helper'
 
+BREW_TAPS_DIR = '/usr/local/Library/Taps/'
+CASK_PACKAGES_DIR = '/opt/homebrew-cask/Caskroom/'
+
 vars = load_variables
 
 vars['brew_taps'].each do |tap|
   user, repo = tap.split('/')
   tap_with_prefix = [user, 'homebrew-' + repo].join('/')
-  describe file(brew_taps_dir + tap_with_prefix) do
+  describe file(BREW_TAPS_DIR + tap_with_prefix) do
     it { should be_directory }
   end
 end
@@ -17,7 +20,7 @@ vars['brew_packages'].each do |package|
 end
 
 vars['cask_packages'].each do |package|
-  describe file(cask_packages_dir + package) do
+  describe file(CASK_PACKAGES_DIR + package) do
     it { should be_directory }
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,14 +7,6 @@ def load_variables
   YAML.load_file(File.expand_path('../../vars/main.yml', __FILE__))
 end
 
-def brew_taps_dir
-  '/usr/local/Library/Taps/'
-end
-
-def cask_packages_dir
-  '/opt/homebrew-cask/Caskroom/'
-end
-
 def expand_home(path)
   path.gsub(/{{ home }}/, File.expand_path('~'))
 end
