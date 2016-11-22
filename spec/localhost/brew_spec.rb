dd7d095b7b5a5c3d600e2b6f08eff6de7cdf19ea
Yuichi Tanikawa <t.yuichi111@gmail.com>
2016-11-22 20:30:43 +0900

    Fix errors

--- spec/localhost/brew_spec.rb
+++ spec/localhost/brew_spec.rb
@@ -1,14 +1,11 @@
 require 'spec_helper'
 
-BREW_TAPS_DIR = '/usr/local/Library/Taps/'
-
 vars = load_variables
 
 vars['brew_taps'].each do |tap|
-  user, repo = tap.split('/')
-  tap_with_prefix = [user, 'homebrew-' + repo].join('/')
-  describe file(BREW_TAPS_DIR + tap_with_prefix) do
-    it { should be_directory }
+  describe command("brew tap | grep #{tap}") do
+    its(:exit_status) { should eq(0) }
+    its(:stdout) { should eq("#{tap}\n") }
   end
 end
 
